62f76c533af7225c187de6c0a5d3f6001b002d1c
Kang, Yubeen <skhu202417129@gmail.com>
2025-10-20 16:09:11 +0900

    five

--- src/k202417129/Main.java
+++ src/k202417129/Main.java
@@ -2,7 +2,8 @@ package k202417129;
 
 public class Main {
 	public static void main(String[] args) { 
-		System.out.println("홍길동");
+		System.out.println("강유빈");
 		System.out.println("안녕하세요");
+		Hello.run();
 }
 }
